eede9e12b348c67648d24015b1d50486157d17e6
Vuthy Panha <panhajack012@gmail.com>
2022-05-14 22:54:19 +0700

    i update on sm:from origin to 412px just to see the responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '412px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {},
   },
   plugins: [],
